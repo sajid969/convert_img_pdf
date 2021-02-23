d0e306ff67b1bdfdcb77bf295be8a49830ddf3b9
sajid <saj.96994@gmail.com>
2021-02-23 17:36:47 +0530

    convert_img_pdf

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn convertimgtopdf.wsgi --log-file -
--- convertimgtopdf/settings.py
+++ convertimgtopdf/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'ckn1xr@72=#h!@8r2#kdh-g&$l3xs+&ps*tk(3vbo&ga&r9oqf'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['convertimgtopdf.herokuapp.com','127.0.0.1']
 
 
 # Application definition
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
